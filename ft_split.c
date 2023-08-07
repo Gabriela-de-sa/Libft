a9ccb086ddad9955a4c4141b81ab4168eb580497
Gabriela De Sa <gde-sa@c1r1p4.42sp.org.br>
2023-08-07 18:05:21 -0300

    libft

--- ft_split.c
+++ ft_split.c
@@ -3,24 +3,97 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriela <gabriela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/06 20:52:19 by gabriela          #+#    #+#             */
-/*   Updated: 2023/08/06 20:56:28 by gabriela         ###   ########.fr       */
+/*   Created: 2023/08/07 11:54:38 by gde-sa            #+#    #+#             */
+/*   Updated: 2023/08/07 18:04:21 by gde-sa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
+
+int	count_words(char const *s, char c)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	while (s[i] && s[i] == c)
+			i++;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			count += 1;
+			while (s[i] == c)
+				i++;
+			if (s[i] == '\0')
+				count -= 1;
+			continue ;
+		}
+		i++;
+	}
+	return (count + 1);
+}
+
+char	string_split(char *s, int start, char *split, int len)
+{
+	split = malloc(len + 1);
+	split = ft_strlcpy(split, &s[start], len);
+	return (split);
+}
+
+// colocar as palavras dentro do split
+char	*put_split(char *s, char c, char *split)
+{
+	int		i;
+	int		index;
+	int		start;
+	int		len;
+
+	i = -1;
+	index = -1;
+	while (split[++i])
+	{
+		while (s[++index])
+		{
+			if (s[index] != c)
+			{
+				start = index;
+				len = 0;
+				while (s[index + len] != c)
+					len++;
+			}
+			if (len > 0)
+				string_split(s, start, &split[i], len);
+		}
+	}
+	split[i] = NULL;
+	return (split);
+}
 
 char	**ft_split(char const *s, char c)
 {
-	
+	int		words;
+	char	**split;
+
+	words = count_words(s, c);
+	split = malloc(words * sizeof(char *) + 1);
+	return (put_split(s, c, split));
+}
+
+int main(void)
+{
+	ft_split("  gabriela      de   sa     ", ' ');
 }
 
 /*
-posso usar o strchr para detectar o caracter que vai ser determinado para o split
 
-malloc vai ser o dobro da string
+s = string para ser split
+c = caracter para ser delimitado
 
+final de array contendo null
 
 */
